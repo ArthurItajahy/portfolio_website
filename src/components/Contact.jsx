2cf78bac1a333255c551947718095f17166a8a80
Arthur Itajahy <canalp3rry@gmail.com>
2024-04-04 15:45:30 -0300

    feat: finish the email function.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
 const Contact = () => {
   const formRef = useRef();
 
@@ -19,8 +20,43 @@ const Contact = () => {
   })
 
   const [loading, setLoading] = useState(false);
-  const handleChange = (e) => { }
-  const handleSubmit = (e) => { }
+  const handleChange = (e) => { 
+    const{name, value} = e.target;
+    setForm({...form, [name]:value})
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send(
+      'service_9xplk2f', 
+      'template_ouzq9tl', 
+      {
+        from_name: form.name,
+        to_name: 'Adrian',
+        from_email: form.email,
+        to_email: 'dev.ai.count@gmail.com',
+        message: form.message,
+
+      },
+      'adePiVi_mTLTs3qL4' // Public Key
+      
+      )
+      .then(() => {
+        setLoading(false);
+        alert("Thank you, I will get back to you as soon as possible.");
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        })
+      }, (error) => {
+        setLoading(false)
+
+        console.log(error);
+        alert("Something went wrong.")
+      
+      })
+   }
 
   return (
     <div
